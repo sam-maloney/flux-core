83ab0590a94c852a9d7b1f003e74c3b8cdd15671
Sam Maloney <56830868+sam-maloney@users.noreply.github.com>
2025-01-17 10:37:09 +0100

    Comment out 1st check in jj.c

--- src/common/libjob/jj.c
+++ src/common/libjob/jj.c
@@ -124,12 +124,14 @@ int jj_get_counts_json (json_t *jobspec, struct jj_counts *jj)
         errno = EINVAL;
         return -1;
     }
+    /*
     if (version != 1) {
         snprintf (jj->error, sizeof (jj->error) - 1,
                  "Invalid version: expected 1, got %d", version);
         errno = EINVAL;
         return -1;
     }
+    */
     /* N.B. attributes.system is generally optional, but
      * attributes.system.duration is required in jobspec version 1 */
     if (json_unpack_ex (jobspec, &error, 0, "{s:{s:{s:F}}}",
