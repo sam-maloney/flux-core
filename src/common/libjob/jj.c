08e33264badf0ebbb4fb2a864305413bc036761e
Sam Maloney <56830868+sam-maloney@users.noreply.github.com>
2025-01-17 15:16:59 +0100

    Modified tests for modified jj.c

--- src/common/libjob/jj.c
+++ src/common/libjob/jj.c
@@ -15,6 +15,7 @@
 #include <errno.h>
 #include <string.h>
 #include <jansson.h>
+#include <flux/core.h>
 
 #include "ccan/str/str.h"
 
@@ -124,14 +125,11 @@ int jj_get_counts_json (json_t *jobspec, struct jj_counts *jj)
         errno = EINVAL;
         return -1;
     }
-    /*
     if (version != 1) {
         snprintf (jj->error, sizeof (jj->error) - 1,
-                 "Invalid version: expected 1, got %d", version);
-        errno = EINVAL;
-        return -1;
+                 "Unsupported version: expected 1, got %d", version);
+        flux_log(NULL, LOG_WARNING, "%s: %s", __FUNCTION__, jj->error);
     }
-    */
     /* N.B. attributes.system is generally optional, but
      * attributes.system.duration is required in jobspec version 1 */
     if (json_unpack_ex (jobspec, &error, 0, "{s:{s:{s:F}}}",
