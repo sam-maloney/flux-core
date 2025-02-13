b079cf815cba106dc7bdf12703fa165983e3f9bb
Sam Maloney <56830868+sam-maloney@users.noreply.github.com>
2025-02-13 10:43:23 +0100

    shell: refactor jobspec fetching during shell init
    Problem: jobspec_parse() function needs information on actual resource
    allocation in order to resolve ranges in jobspec, but is currently just
    getting jobspec string.
    
    As an initial step to simplify the dataflow to jobspec_parse(), move
    the initial conversion of jobspec from string to json and field
    extraction from jobspec_parse() into lookup_jobspec_get().

--- src/shell/jobspec.h
+++ src/shell/jobspec.h
@@ -22,13 +22,16 @@ struct jobspec {
     int cores_per_slot;         // number of cores per task slot
     int slots_per_node;         // number of slots per node (-1=unspecified)
     int node_count;             // number of nodes (-1=unspecified)
-    json_t *command;
     const char *cwd;
+    json_t *resources;
+    json_t *command;
+    json_t *count;
     json_t *environment;
     json_t *options;            // attributes.system.shell.options, if any
 };
 
-struct jobspec *jobspec_parse (const char *jobspec, json_error_t *error);
+int jobspec_parse (struct jobspec *job, json_error_t *error);
+void set_error (json_error_t *error, const char *fmt, ...);
 void jobspec_destroy (struct jobspec *job);
 
 #endif /* !_SHELL_JOBSPEC_H */
